cb974f11637472bfe04988430c9f7c3eadab916a
jareth <86022685+jarethxin@users.noreply.github.com>
2024-06-17 17:30:49 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,18 +1,13 @@
 require("dotenv").config();
 const sql = require("mssql");
 
-console.log(`DB_USER: ${process.env.DB_USER}`);
-console.log(`DB_PASSWORD: ${process.env.DB_PASSWORD}`);
-console.log(`DB_SERVER: ${process.env.DB_SERVER}`);
-console.log(`DB_DATABASE: ${process.env.DB_DATABASE}`);
-
 const dbConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   server: process.env.DB_SERVER,
   database: process.env.DB_DATABASE,
   options: {
-    encrypt: true,
+    encrypt: false,
     enableArithAbort: true,
   },
   connectionTimeout: 30000,
