91d5f9f2a133905debd232f4d6f5a94e69a08863
jareth <86022685+jarethxin@users.noreply.github.com>
2025-04-24 18:03:55 -0500

    up
    obtención de archivo pdf de liquidación funcional; falta implementar en la app

--- controllers/liquidacionesController.js
+++ controllers/liquidacionesController.js
@@ -1,5 +1,7 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const path = require('path');
+const fs = require('fs');
 const {
     getLiquidacionesListByNoOperador,
   } = require("../models/liquidacionesModel");
@@ -30,6 +32,26 @@ const get_liquidaciones_by_no_operador = async (req, res) => {
   }
 };
 
+const get_liquidacion_pdf_by_operador_and_liquidacion = async (req, res) => {
+  const { noOperador, folioLiquidacion } = req.params;
+  const basePath = process.env.RUTA_ARCHIVOS_LIQUIDACIONES;
+  
+  const filePath = path.join(basePath, noOperador, `L${folioLiquidacion}.pdf`);
+  
+  if (fs.existsSync(filePath)) {
+    res.setHeader('Content-Type', 'application/pdf');
+    res.sendFile(filePath, (err) => {
+      if (err) {
+        console.error(`Error enviando archivo con el nombre: L${folioLiquidacion}.pdf`, err);
+        res.status(500).send('Error al enviar el documento');
+      }
+    });
+  } else {
+    res.status(404).json({ message: 'Archivo PDF no encontrado' });
+  }
+}
+
 module.exports = {
-    get_liquidaciones_by_no_operador
+    get_liquidaciones_by_no_operador,
+    get_liquidacion_pdf_by_operador_and_liquidacion
 };
--- routes/liquidacionesRoutes.js
+++ routes/liquidacionesRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const {get_liquidaciones_by_no_operador} = require('../controllers/liquidacionesController');
+const {get_liquidaciones_by_no_operador, get_liquidacion_pdf_by_operador_and_liquidacion} = require('../controllers/liquidacionesController');
 const router = express.Router();
 
 router.get('/operador', get_liquidaciones_by_no_operador);
+
+router.get('/:noOperador/pdf/:folioLiquidacion', get_liquidacion_pdf_by_operador_and_liquidacion)
 
 module.exports = router;
